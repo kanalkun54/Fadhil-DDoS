832614738bcf4ad1bf89b6154fd92ad665c13a06
kanalkun54 <kanalkun54@gmail.com>
2025-09-16 10:42:52 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("apt install python")
+    os.system("apt install python2")
+    os.system("pip install fade")
+    os.system("git pull")
+    
+elif c == "1":
+    os.system("apt install python")
+    os.system("apt install python2")
+    os.system("pip3 install fade")
+    os.system("git pull")
+print("Done.")
